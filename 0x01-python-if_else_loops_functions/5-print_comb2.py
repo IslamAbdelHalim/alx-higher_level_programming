de21e2931bfd42dc228af1b60f4fba59f987b767
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-11-01 16:08:14 +0200

    a

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print(f"{str(i).zfill(2)}, ", end="")
+        print(f"0{i}, " if i < 10 else f"{i}, ", end="")
     else:
         print(i)

e97a59a591b06635bfd41834e36da35a9cb057de
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-11-05 15:07:17 +0200

    a

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idex):
+    if idex < 0:
+        return None
+    elif idex > len(my_list):
+        return None
+    else:
+        return my_list[idex]
+
+
+# None: is a special value that represents the absence of a
+# value or the concept of nothingness.

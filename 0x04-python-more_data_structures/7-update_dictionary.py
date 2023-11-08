512a652a58871bf33387f0bc7a0e0da32ce79e73
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-11-08 21:18:21 +0200

    a

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    if not key:
+        return a_dictionary
     for ky in a_dictionary.copy():
         if ky == key:
             a_dictionary[ky] = value

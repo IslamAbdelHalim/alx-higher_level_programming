4212a79a09444f948b4b49430aa0a3e41d507390
IslamAbdelHalim <eslamelbeltagye1111@gmail.com>
2023-11-27 19:18:55 +0200

    a

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0-Square is a class
+class define a square
+"""
+
+
+class Square:
+    """Square is an empty class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+"""1-square is a module"""
+
+
+class Square:
+    """size are the size of square
+
+    The __init__ method is used to initialize the class
+    """
+    def __init__(self, size):
+        self.__size = size

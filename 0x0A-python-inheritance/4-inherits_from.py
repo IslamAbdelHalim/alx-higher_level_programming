2e09b30c9b461e738f2697c9b16d305587332c46
IslamAbdelHalim <eslamelbeltagye1111@gmail.com>
2023-12-04 21:09:43 +0200

    a

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""subclass module"""
+
+
+def inherits_from(obj, a_class):
+    """inherits function"""
+    
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False

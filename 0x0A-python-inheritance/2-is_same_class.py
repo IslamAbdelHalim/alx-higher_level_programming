3383486f0bb0b38c9baadbe0e200061f94d20916
IslamAbdelHalim <eslamelbeltagye1111@gmail.com>
2023-12-04 20:55:02 +0200

    a

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/use/bin/python3
+"""isinistance module"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function that return True if
+    The obj is inistance of a_class
+    or false if is not
+    """
+
+    return type(obj) is a_class

dfee6704690fb6fd0ecf4057071ac9410867b6a5
IslamAbdelHalim <eslamelbeltagye1111@gmail.com>
2023-12-06 00:20:32 +0200

    a

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+"""write a string to a text file"""
+
+def write_file(filename="", text=""):
+    """
+    function that write a text into a file
+
+    Args:
+        filename: The name of file
+
+        text: The text which we adding
+    """
+    with open(filename, 'w', encoding="UTF-8") as file:
+        return file.write(text)

7c5bbf8d7c5c2200573d2e1f70115e9f18cf05c1
IslamAbdelHalim <eslamelbeltagye1111@gmail.com>
2023-12-06 02:21:39 +0200

    a

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,7 +2,6 @@
 """student module"""
 
 
-
 class Student:
     """Class student"""
     def __init__(self, first_name, last_name, age):
@@ -10,6 +9,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """retrieves a dictionary representation
         of a Student instance"""

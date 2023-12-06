a38b84660dce8c110f1b151166d3167407006a3c
IslamAbdelHalim <eslamelbeltagye1111@gmail.com>
2023-12-06 02:38:53 +0200

    a

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""pascal triangle"""
+
+
+def pascal_triangle(n):
+    """function that make pascal tringle"""
+
+    if n <= 0:
+        return []
+    pascal = []
+
+    for row in range(n):
+        current_row = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                current_col.append(1)
+            else:
+                current_row.append
+                (pascal[row-1][col - 1] + pascal[row - 1][col])
+        pascal.append(current_row)
+        return pascal

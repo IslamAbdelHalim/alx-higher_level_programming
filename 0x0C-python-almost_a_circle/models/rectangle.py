b7c87a9baa7bd9a32cd8748d8380c868aee0884a
IslamAbdelHalim <eslamelbeltagye1111@gmail.com>
2023-12-11 21:25:37 +0200

    modify height

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(Base):
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height <= 0:
-            raise ValueError("heigth must be > 0")
+            raise ValueError("height must be > 0")
         self.height = height
 
         if type(x) is not int:

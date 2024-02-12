ae65ee2e2b70ea71f46091d328d7cdabe8bcb65f
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-02-12 19:31:55 +0200

    value

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const len = args.length;
+
+if (len === 2) {
+  console.log('No argument');
+} else {
+  for (let i = 2; i < len; i++) {
+    console.log(args[i]);
+  }
+}

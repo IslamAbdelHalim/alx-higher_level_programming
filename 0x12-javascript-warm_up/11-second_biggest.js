e26e613567a6b7dd5a0c3746896989189848b370
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-02-12 23:43:24 +0200

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args.length === 2 || args.length === 3) {
+  console.log(1);
+} else {
+  // sort the array
+  const SortArgs = args.sort();
+  // know the length
+  const len = SortArgs.length;
+  // search the second biggest integer
+  console.log(SortArgs[len - 2]);
+}

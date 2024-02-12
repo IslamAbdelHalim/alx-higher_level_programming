69d8fde8620067496a9a8cefe90b7e03c29ca5fd
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-02-12 23:47:28 +0200

    convert to number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,10 +5,13 @@ const args = process.argv;
 if (args.length === 2 || args.length === 3) {
   console.log(0);
 } else {
+
   // sort the array
   const SortArgs = args.sort();
+
   // know the length
   const len = SortArgs.length;
+
   // search the second biggest integer
-  console.log(SortArgs[len - 2]);
+  console.log(Number(SortArgs[len - 2]));
 }

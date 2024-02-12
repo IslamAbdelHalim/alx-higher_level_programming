fde81fd31cf251a8f513dbf7fcaa1311a27c73da
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-02-13 01:26:33 +0200

    sorting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,8 @@ if (args.length < 4) {
   console.log(0);
 } else {
   // sort the array
-  const SortArgs = args.sort();
+  const SortArgs = args.sort((a, b) => a - b).slice(2).map(Number);
+  console.log(SortArgs);
 
   // know the length
   const len = SortArgs.length;

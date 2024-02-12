2048a03d67088a69b487982a7457dcff0e0aa3a8
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-02-12 19:16:46 +0200

    a

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.length;
 if (args === 2) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (args === 3) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }

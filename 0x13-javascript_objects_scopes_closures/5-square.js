81a8598ef599e3de27a531f60f756c018fbb060a
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-02-13 22:46:57 +0200

    a

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,11 +4,8 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    if (size > 0) {
-      super(size, size);
-      this.size = size;
-    }
-
+    super(size, size);
+    this.size = size;
   }
 }
 

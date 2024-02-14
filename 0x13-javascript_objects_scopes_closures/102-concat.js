c4f72d4792732a8cd1415deb8cc8d6f273de0a3a
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-02-14 12:34:03 +0200

    remove new line

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const args = process.argv;
 
-const contant1 = fs.readFileSync(args[2]).toString() + '\n';
-const contant2 = fs.readFileSync(args[3]).toString() + '\n';
+const contant1 = fs.readFileSync(args[2]).toString();
+const contant2 = fs.readFileSync(args[3]).toString();
 
 fs.writeFileSync(args[4], contant1 + contant2);

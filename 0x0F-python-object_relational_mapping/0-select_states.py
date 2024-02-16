f561ede4b579efa5b5a51d0b0767a988fafdcfb0
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-02-16 18:57:57 +0200

    apply sys statment

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,15 @@ Write a script that lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
+import sys
 
 # Create a connection with the database
 db = MySQLdb.connect(
         host="localhost",
-        user="root",
-        passwd="Islam2908$",
-        database="hbtn_0e_0_usa")
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+        port=3306)
 
 # Create a cursor
 cursor = db.cursor()
@@ -21,3 +23,6 @@ cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 # fetchall
 for value in cursor.fetchall():
     print(value)
+
+cursor.close();
+db.close();

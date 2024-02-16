40cf711c9e73e33090c64f67a39e52d4f87720bb
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-02-16 19:03:16 +0200

    if name

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,23 +6,24 @@ Write a script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-# Create a connection with the database
-db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database=sys.argv[3],
-        port=3306)
+if __name__ == "__main__":
+    # Create a connection with the database
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            database=sys.argv[3],
+            port=3306)
 
-# Create a cursor
-cursor = db.cursor()
+    # Create a cursor
+    cursor = db.cursor()
 
-# Execute the statment
-cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    # Execute the statment
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-# fetchall
-for value in cursor.fetchall():
-    print(value)
+    # fetchall
+    for value in cursor.fetchall():
+        print(value)
 
-cursor.close();
-db.close();
+    cursor.close()
+    db.close()

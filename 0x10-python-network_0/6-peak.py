201c866ec9599371a51a6320eb0fc0713a6d871b
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-03-02 00:14:54 +0200

    a

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,13 +13,12 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 1:
         return list_of_integers[0]
 
-    mid = len(list_of_integers) // 2
+    left, right = 0, len(list_of_integers) - 1
 
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-       list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-
-    if list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid+1:])
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return list_of_integers[left]

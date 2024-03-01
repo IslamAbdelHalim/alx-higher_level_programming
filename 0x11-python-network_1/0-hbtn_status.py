c186afb01e05402465158949c10c0eba7e6d0774
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-03-02 01:47:02 +0200

    a

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         data = resp.read()
         data = data.decode('utf-8')
-
-    print("Body response:")
-    print("    - type: {}".format(type(data)))
-    print("    - content: {}".format(data))
-    print("    - utf8 content: {}".format(data))
+        print("Body response:")
+        print("    - type: {}".format(type(data)))
+        print("    - content: {}".format(data))
+        print("    - utf8 content: {}".format(data))

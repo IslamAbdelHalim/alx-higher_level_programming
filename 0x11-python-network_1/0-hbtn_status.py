ce79df983f3fcd0720aff61bdf2732d528c0efa2
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-03-02 01:44:55 +0200

    fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        data = resp.read()
+        data = data.decode('utf-8')
+
+    print("Body response:")
+    print("    - type: {}".format(type(data)))
+    print("    - content: {}".format(data))
+    print("    - utf8 content: {}".format(data))

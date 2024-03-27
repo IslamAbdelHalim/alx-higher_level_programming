cf76320871982147b66b305cbab5353ea1ecca0c
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-03-27 22:45:16 +0200

    a

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,8 +4,8 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, res, body) => {
-  const movies = JSON.parse(body).results;
   let num = 0;
+  const movies = JSON.parse(body).results;
   movies.forEach(element => {
     if (element.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
       num++;
